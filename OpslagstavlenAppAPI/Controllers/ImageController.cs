71292145e1d5e9e033c5faaa1d3d797291e0d2d9
Morten Vestergaard <mbvestergaard@hotmail.com>
2022-11-20 15:24:28 +0100

    Add project files.

---
+++ OpslagstavlenAppAPI/Controllers/ImageController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OpslagstavlenAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        // GET: api/<ImageController>
+        [HttpGet]
+        [Route("GetImages")]
+        public IEnumerable<string> Get()
+        {
+            string imagePath = @"C:\Users\mort286f\Pictures\OpslagstavleImages";
+            List<string> images = Directory.GetFiles(imagePath, "*.*", SearchOption.TopDirectoryOnly).ToList();
+            List<string> resultImages = new List<string>();
+
+            foreach (string image in images)
+            {
+                resultImages.Add(Convert.ToBase64String(System.IO.File.ReadAllBytes(image)));
+            }
+            return resultImages;
+        }
+
+        //[HttpPost]
+        //[Route("PostImage")]
+        //public IEnumerable<string> Post()
+        //{
+
+        //}
+    }
+}
